3d51691adafbd7de04e3dcb174255e0cf3bf6925
Genilson Alan <allanbb072@gmail.com>
2026-03-30 18:27:10 -0300

    inserindo animação do botão de estatus

--- script.js
+++ script.js
@@ -1,46 +1,26 @@
-fetch('betha.json')
-.then(response => response.json())
-.then(data => {
-    let id = 1
-    const container = document.getElementById("list")
-    //console.log(data)
-    //console.log(data[0])
-    data.forEach(item => {
-        const div = document.createElement("div")
-        div.innerHTML = `
-            <div class="reservado">
-                <p>${item["Resv.Normal"]}</p>
-            </div>
-            <div class="posicao">
-                <p>${item.Endereco}</p>
-            </div>
-            <div class="caixas">
-                <p>${item["Dispon.Exped."]/10}</p>
-            </div>
-            <div class="pacotes">
-                <p>${0}</p>
-            </div>
-            <div class="macos">
-                <p>${0}</p>
-            </div>
-            <div class="sku">
-                <p>${item.SKU}</p>
-            </div>
-            <div class="descricao">
-                <p>${item.Descricao}</p>
-            </div>
-            <div class="observacao">
-                <p>${"OK"}</p>
-            </div>
-            <div class="status">
-                <button class="status-btn" id="item${id}">OK</button>
-            </div>
-            <div class="edite">
-                <button class="edit-btn" id="itemID${id}">Editar</button>
-            </div>
-        `
-        id = id + 1
-        container.appendChild(div)        
-    });
+window.addEventListener("load", function(){
+        const dados = document.getElementById("list")
+        const arrayDados = [...dados.children]
+        console.log(arrayDados.length)
+
+        let id = 1
+
+        for(id; id <= arrayDados.length; id++){
+                const itemOne = document.getElementById(`item${id}`)
+
+                itemOne.addEventListener("click", () =>{
+                        const styleCss = this.window.getComputedStyle(itemOne)
+                        const buttonBkg = styleCss.backgroundColor
+
+                        if(buttonBkg === "rgb(255, 69, 0)" || buttonBkg === "orangered"){
+                                itemOne.style.backgroundColor = "green"
+                                itemOne.value = 1
+                        }else if(buttonBkg === "rgb(0, 128, 0)" || buttonBkg === "green"){
+                                itemOne.style.backgroundColor = "orangered"
+                                itemOne.value = 0
+                        }                
+                })
+        }
+
+        
 })
-.catch(error => console.error("Erro: ", error));
