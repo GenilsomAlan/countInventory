dce32bf29cf65d8d819405a20477546b6687fae8
G. Allan <90303422+GenilsomAlan@users.noreply.github.com>
2026-03-30 16:39:50 -0300

    Add script to fetch and display data from JSON

---
+++ script.js
@@ -0,0 +1,46 @@
+fetch('betha.json')
+.then(response => response.json())
+.then(data => {
+    let id = 1
+    const container = document.getElementById("list")
+    //console.log(data)
+    //console.log(data[0])
+    data.forEach(item => {
+        const div = document.createElement("div")
+        div.innerHTML = `
+            <div class="reservado">
+                <p>${item["Resv.Normal"]}</p>
+            </div>
+            <div class="posicao">
+                <p>${item.Endereco}</p>
+            </div>
+            <div class="caixas">
+                <p>${item["Dispon.Exped."]/10}</p>
+            </div>
+            <div class="pacotes">
+                <p>${0}</p>
+            </div>
+            <div class="macos">
+                <p>${0}</p>
+            </div>
+            <div class="sku">
+                <p>${item.SKU}</p>
+            </div>
+            <div class="descricao">
+                <p>${item.Descricao}</p>
+            </div>
+            <div class="observacao">
+                <p>${"OK"}</p>
+            </div>
+            <div class="status">
+                <button class="status-btn" id="item${id}">OK</button>
+            </div>
+            <div class="edite">
+                <button class="edit-btn" id="itemID${id}">Editar</button>
+            </div>
+        `
+        id = id + 1
+        container.appendChild(div)        
+    });
+})
+.catch(error => console.error("Erro: ", error));
