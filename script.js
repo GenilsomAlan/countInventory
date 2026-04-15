e1d9769392b2ae58728132322eb782b2349ad287
Genilson Alan <allanbb072@gmail.com>
2026-04-14 23:37:02 -0300

    ...

--- script.js
+++ script.js
@@ -9,7 +9,64 @@ const verificarEncerramentoDaContagem = () => {
 
 const btnDivergences = document.getElementById("showDivergences")
 btnDivergences.addEventListener("click", () =>{
-    coletarDivergencias()
+    const divergencias = coletarDivergencias()
+
+    document.body.innerHTML = htmlDivergencias()
+
+    const container = document.getElementById("resultadoDivergencias")
+
+    if(divergencias.length === 0){
+        container.innerHTML = "<p>Não foram encontradas divergências</p>"
+        return
+    }
+
+    divergencias.forEach(item =>{
+        const div = document.createElement("div")
+        div.classList.add("linha")
+        div.innerHTML = `
+            <div class="reservado">
+                <p>${item.reservado}</p>
+            </div>
+
+            <div class="posicao">
+                <p>${item.posicao}</p>
+            </div>
+
+            <div class="caixas">
+                ${item.caixas}
+            </div>
+
+            <div class="pacotes" ">
+                ${item.pacotes}
+            </div>
+
+            <div class="macos">
+                ${item.macos}
+            </div>
+
+            <div class="sku">
+                <p>${item.sku}</p>
+            </div>
+
+            <div class="descricao">
+                <p>${item.descricao}</p>
+            </div>
+
+            <div class="observacao">
+                <p>${item.observacao}</p>
+            </div>
+
+            <div class="status">
+                <button class="status-btn" data-editing="false">*</button>
+            </div>
+
+            <div class="edite">
+                <button class="edit-btn" data-editing="false">Editar</button>
+            </div>
+        `
+        container.appendChild(div)
+
+    })
 })
 
 const coletarDivergencias = () => {
@@ -42,4 +99,59 @@ const coletarDivergencias = () => {
         }
     })
     return divergencias
+}
+
+const htmlDivergencias = () => {
+    return `    
+     <header>
+        <h1>Divergências Encontradas</h1>
+    </header>
+
+    <main>
+        <div class="cabecalho">
+
+            <div class="reservado">
+                <p>Reservado</p>
+            </div>
+
+            <div class="posicao">
+                <p>Posição</p>
+            </div>
+
+            <div class="caixas">
+                <p>Caixas</p>
+            </div>
+
+            <div class="pacotes">
+                <p>Pacotes</p>
+            </div>
+
+            <div class="macos">
+                <p>Maços</p>
+            </div>
+
+            <div class="sku">
+                <p>SKU</p>
+            </div>
+
+            <div class="descricao">
+                <p>Descrição</p>
+            </div>
+
+            <div class="observacao">
+                <p>Observação</p>
+            </div>
+
+            <div class="status">
+                <p>Status</p>
+            </div>
+
+            <div class="edite">
+                <p>Editar</p>
+            </div>
+        </div>
+
+        <div id="resultadoDivergencias"></div>
+    </main>
+    `
 }
